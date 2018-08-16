359e179d41418f64d2d8989cbf931be19fb9c8d3
Jason Johnson <jjohnson@rocketsoftware.com>
2018-08-15 23:20:24 -0500

    Creating a new option to delete (aka archive) checked items

--- index.js
+++ index.js
@@ -57,6 +57,10 @@ const taskbookCLI = (input, flags) => {
     return taskbook.moveBoards(input);
   }
 
+  if (flags.deleteChecked) {
+    return taskbook.clearItems(input);
+  }
+
   taskbook.displayByBoard();
   return taskbook.displayStats();
 };
